35d152c5958241bf9be06669377e6e8deccd4a02
sloth-hub <madamhh@gmail.com>
2020-11-13 19:20:17 +0900

    Setup Router and Use firebase auth

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import AppRouter from './Router';
+import {authService} from "fbase";
+
+const App = () => {
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Slothwitter </footer>
+    </>
+  );
+}
+
+export default App;
